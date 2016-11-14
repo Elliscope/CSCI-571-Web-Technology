5fb2a3f6b454b253f5e5b30030e6a4b243e8c871
Elliscope Fang <mingzhef@usc.edu>
2016-11-14 14:19:05 -0800

    fix data loading misorder issues.angular ajax has different loading api

--- hw8/dir_script.js
+++ hw8/dir_script.js
@@ -1,25 +1,18 @@
-// Code goes here
+
 
 var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
 
-function MyController($scope) {
+function MyController($scope,$http) {
 
   $scope.currentPage = 1;
   $scope.pageSize = 10;
   $scope.people_data= [];
 
     var php_path = "backend/index.php?";
-    $.ajax({
-      url: php_path,
-      data: {category: "legistlator",chamber: "house",keyword: "CA", }
-    })
-      .done(function( data ) {
-            var doc = eval('(' + data + ')');
-            $scope.people_data = doc.results;
-              console.log($scope.people_data);
-        });
-
-  //replace the meals item with the data object
+     $http.get("backend/index.php?category=legistlator&chamber=house&keyword=CA")
+    .then(function(response) {
+        $scope.people_data = response.data.results;
+    });
 
   $scope.pageChangeHandler = function(num) {
       console.log('list page changed to ' + num);
