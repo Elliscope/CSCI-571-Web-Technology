ad32858dea472d27fece0653cc2f8dfd24291ddb
Elliscope Fang <mingzhef@usc.edu>
2016-11-14 15:56:08 -0800

    added rough sort for state

--- hw8/dir_script.js
+++ hw8/dir_script.js
@@ -8,11 +8,32 @@ function MyController($scope,$http) {
   $scope.pageSize = 10;
   $scope.people_data= [];
 
+
     var php_path = "backend/index.php?";
      $http.get("backend/index.php?category=legistlator&chamber=house&keyword=CA")
     .then(function(response) {
         $scope.people_data = response.data.results;
+        console.log($scope.people_data);
+
+                for (var i = 0; i <= response.data.count; i++) {
+                  $scope.people_data[i].party = $scope.people_data[i].party.toLowerCase();
+
+                  if($scope.people_data[i].chamber =="house"){
+                    $scope.people_data[i].chamber = "h.png";
+                  }else{
+                    $scope.people_data[i].chamber = "s.svg";
+                  }
+
+                  if($scope.people_data[i].district == " "){
+                    $scope.people_data[i].district = "N.A";
+                  }else{
+                    $scope.people_data[i].district =  "District"+$scope.people_data[i].district;
+                  }
+
+                }
     });
+
+
 
   $scope.pageChangeHandler = function(num) {
       console.log('list page changed to ' + num);
