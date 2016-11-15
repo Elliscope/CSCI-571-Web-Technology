c258730d840dbe03089169d751749efacc69918f
Elliscope Fang <mingzhef@usc.edu>
2016-11-14 22:52:33 -0800

    viewDetails data loading bioguide_data and bill_data done

--- hw8/backend/index.php
+++ hw8/backend/index.php
@@ -4,17 +4,30 @@
 $category = $_GET['category'];
 $chamber = $_GET['chamber'];
 $keyword = $_GET['keyword'];
+$bioguide = $_GET['bioguide'];
+
 
 // echo $chamber;
 // echo $keyword ;
 // echo $category;
 
 if($category=="legistlator"){
+	if($bioguide!=null){
+		return legislatorBioguideSearch($bioguide,$chamber,$keyword);
+	}
 	return legislatorSearch($chamber,$keyword);
+
 }else if($category=="bills"){
+	if($bioguide!=null){
+		return legistlatorBillSearch($bioguide);
+	}
 	return BillsSearch($chamber,$keyword);
 }else if($category=="committees"){
+	if($bioguide!=null){
+		return legistlatorCommitteesSearch($bioguide);
+	}
 	return CommitteesSearch($chamber,$keyword);
+
 }
 
 
@@ -27,11 +40,36 @@ function legislatorSearch($chamber,$keyword){
 	}else{
 		$url = "http://104.198.0.197:8080/legislators?chamber=".$chamber."&state=".$keyword."&apikey=".$apikey;
 	}
-	
 	$res = file_get_contents($url);
 	echo $res;
 	return $res;
 }
+
+function legislatorBioguideSearch($bioguide,$chamber,$keyword){
+	$apikey = 'ad112f71df2e4109864ee87613db82d8';
+	$url = "http://104.198.0.197:8080/legislators?chamber=".$chamber."&state=".$keyword."&bioguide_id=".$bioguide."&apikey=".$apikey;
+	$res = file_get_contents($url);
+	echo $res;
+	return $res;
+}
+
+
+
+function legistlatorBillSearch($bioguide){
+	$apikey = 'ad112f71df2e4109864ee87613db82d8';
+	$url = "http://104.198.0.197:8080/bills?per_page=5&apikey=".$apikey."&sponsor.bioguide_id=".$bioguide;
+	$res = file_get_contents($url);
+	echo $res;
+	return $res;
+}
+function legistlatorCommitteesSearch($bioguide){
+	$apikey = 'ad112f71df2e4109864ee87613db82d8';
+	$url = "http://104.198.0.197:8080/committees?per_page=5&"."&apikey=".$apikey."&member_ids=".$bioguide;
+	$res = file_get_contents($url);
+	echo $res;
+	return $res;
+}
+
 
 
 function CommitteesSearch($chamber,$keyword){
--- hw8/dir_script.js
+++ hw8/dir_script.js
@@ -14,7 +14,6 @@ function SortByStateController($scope,$http) {
     .then(function(response) {
         $scope.people_data = response.data.results;
         console.log($scope.people_data);
-
                 for (var i = 0; i <= response.data.count; i++) {
                   $scope.people_data[i].party = $scope.people_data[i].party.toLowerCase();
 
@@ -25,20 +24,66 @@ function SortByStateController($scope,$http) {
                     $scope.people_data[i].chamber_img = "s.svg";
                     $scope.people_data[i].chamber = "Senate";
                   }
-
                   if($scope.people_data[i].district == null){
                     $scope.people_data[i].district_name = "N.A";
                   }else{
                     $scope.people_data[i].district_name =  "District "+$scope.people_data[i].district;
                   }
-
                 }
     });
 
-
-
   $scope.pageChangeHandler = function(num) {
       console.log('list page changed to ' + num);
+  };
+
+  $scope.viewDetails = function(bioguide,chamber,state){
+    console.log("HERE INSDIE THE VIEW DETAILS FUNCTION");
+    console.log(bioguide);
+    console.log(chamber);
+    console.log(state);
+
+    //have the bioguide value. Call three function to get the info separately.
+
+    //reget the personal info: 
+     $http.get("backend/index.php?category=legistlator&chamber="+chamber.toLowerCase()+"&keyword="+state+"&bioguide="+bioguide)
+    .then(function(response) {
+        console.log("HERE IS THE BIOGUIDE SEARCH FUNCTION");
+        $scope.bio_basic = response.data.results;
+        console.log($scope.bio_basic);
+                for (var i = 0; i <= response.data.count; i++) {
+                  $scope.bio_basic[i].party = $scope.bio_basic[i].party.toLowerCase();
+
+                  if($scope.bio_basic[i].chamber =="house"){
+                    $scope.bio_basic[i].chamber_img = "h.png";
+                    $scope.bio_basic[i].chamber = "House";
+                  }else{
+                    $scope.bio_basic[i].chamber_img = "s.svg";
+                    $scope.bio_basic[i].chamber = "Senate";
+                  }
+                  if($scope.bio_basic[i].district == null){
+                    $scope.bio_basic[i].district_name = "N.A";
+                  }else{
+                    $scope.bio_basic[i].district_name =  "District "+$scope.bio_basic[i].district;
+                  }
+                }
+    }); 
+
+  //get the bills that the legistlator sponsors
+  $http.get("backend/index.php?category=bills&bioguide="+bioguide)
+    .then(function(response) {
+        console.log("HERE IS IS THE BILLS SEARCH FUNCTION");
+        $scope.bio_bills = response.data.results;
+        console.log($scope.bio_bills);
+    }); 
+
+
+  //get the committee that the legistlator belongs to
+    // $http.get("backend/index.php?category=bills&bioguide="+bioguide)
+    // .then(function(response) {
+    //     console.log("HERE IS IS THE BILLS SEARCH FUNCTION");
+    //     $scope.bio_bills = response.data.results;
+    //     console.log($scope.bio_bills);
+    // }); 
   };
 
 }
@@ -48,6 +93,9 @@ function SortByStatePageController($scope) {
     console.log('going to page ' + num);
   };
 }
+
+
+
 
 myApp.controller('SortByStateController', SortByStateController);
 myApp.controller('SortByStatePageController', SortByStatePageController);
